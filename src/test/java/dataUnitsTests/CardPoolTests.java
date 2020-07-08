e98e6f07f9d19a224e4a219027bfe211e1bbded4
fmn0012300 <fmn4@uw.edu>
2020-07-08 15:47:03 -0700

    edited documentation

---
+++ src/test/java/dataUnitsTests/CardPoolTests.java
@@ -0,0 +1,21 @@
+package dataUnitsTests;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class CardPoolTests {
+
+    //Just an example of how to write test code, it does not work, remove it later
+    /**
+     * Test whether campus map is created correctly  //write down what this test is about
+     */
+    @Test //important
+    public void testConstructor() { // do the tests here, note: each method only test for 1 aspect of a method
+        // i.e. expect at least 3 - 5 unit test per method in implementation
+        // think of edge cases, wrong input values, general values
+        // Have at least 2 tests that test a series of method call together.
+        // USE ASSERT!
+        // .e.g assertEquals, assertNotEquals etc.. for asserting an exception to be thrown is a bit different
+    }
+
+}
