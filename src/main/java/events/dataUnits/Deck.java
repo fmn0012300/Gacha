005078d06a18dd4384ebd05a74fe02d8f83eb945
junzh99 <junchao@uw.edu>
2020-07-11 15:48:23 -0700

    implementation first draft

--- src/main/java/events/dataUnits/Deck.java
+++ src/main/java/events/dataUnits/Deck.java
@@ -1,6 +1,7 @@
 package events.dataUnits;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 //this is a mutable class represents a deck of cards
 public class Deck {
@@ -15,10 +16,24 @@ public class Deck {
     }
 
     public void addCard(Cards card, int num) {
-
+        if (deck.containsKey(card)){
+            deck.put(card, deck.get(card)+num);
+        }else {
+            deck.put(card, num);
+        }
     }
 
     public void removeCard(Cards card, int num) {
-
+        if (!deck.containsKey(card)){
+            throw new NoSuchElementException();
+        }
+        int value=deck.get(card)-num;
+        if (value<0){
+            throw new IllegalArgumentException();
+        }else if (value==0){
+            deck.remove(card);
+        }else {
+            deck.put(card, value);
+        }
     }
 }
